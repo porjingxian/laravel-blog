ab9fe67a428eb759c1fcfe3a1f8ba5ebbb014e56
Jing Xian Por <jingxian.por@v6technology.net>
2023-05-08 15:33:55 +0800

    Commit message

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,11 @@
 @extends('components/layout')
 @section('content')
-<!-- iterate over each element of the $posts array, assign its value to the $post on each iteration -->
-    @foreach ($posts as $post) 
-    
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-            {{ $post->title }}
-            </a>
-        </h1>
-        <p>
-        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
-        <div>
-            {!! $post->excerpt !!}
-        </div>
-        
-    </article>
-    @endforeach
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            @if ($posts->count())
+                <x-posts-grid :posts="$posts" />
+            @else
+                <p class="text-center">No Posts Yet. Please Check Back Later.</p>
+            @endif
+        </main>
 @endsection
